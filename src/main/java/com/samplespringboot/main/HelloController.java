c53208971b0ceefde48678aeaa726b591da2ef4f
Nagaraj <NNagarajece31@gmail.com>
2020-02-22 22:01:26 +0530

    Initial Commit

---
+++ src/main/java/com/samplespringboot/main/HelloController.java
@@ -0,0 +1,15 @@
+package com.samplespringboot.main;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    String hello() {
+        return "Hello World!";
+    }
+
+
+}
